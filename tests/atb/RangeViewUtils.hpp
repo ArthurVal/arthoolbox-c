797bf8e13dc7d0a106843ba42505aa3e3ba90379
Arthur Valiente <valiente.arthur@gmail.com>
2024-01-16 19:02:20 +0100

    Add some tests to RangeView

--- tests/atb/RangeViewUtils.hpp
+++ tests/atb/RangeViewUtils.hpp
@@ -9,6 +9,99 @@
 #include <string_view>
 #include <type_traits>
 
+namespace meta {
+
+template <class... T> struct TypeList;
+
+/////////////////////////////////////////////////////////////////////////////
+//                                   Combine                               //
+/////////////////////////////////////////////////////////////////////////////
+template <class T, class U> struct Combine;
+
+template <template <class...> class TypeHolder, class... FirstTypes,
+          class... SecondTypes>
+struct Combine<TypeHolder<FirstTypes...>, TypeHolder<SecondTypes...>> {
+  using type = TypeHolder<FirstTypes..., SecondTypes...>;
+};
+
+template <template <class...> class TypeHolder, class... Types, class T>
+struct Combine<TypeHolder<Types...>, T> {
+  using type = TypeHolder<Types..., T>;
+};
+
+template <template <class...> class TypeHolder, class... Types, class T>
+struct Combine<T, TypeHolder<Types...>> {
+  using type = TypeHolder<T, Types...>;
+};
+
+template <class T, class U> using Combine_t = typename Combine<T, U>::type;
+
+/////////////////////////////////////////////////////////////////////////////
+//                              SwapContainer                              //
+/////////////////////////////////////////////////////////////////////////////
+template <class Source, template <class...> class Dest> struct SwapContainer;
+
+template <template <class...> class Source, template <class...> class Dest,
+          class... Types>
+struct SwapContainer<Source<Types...>, Dest> {
+  using type = Dest<Types...>;
+};
+
+template <class Source, template <class...> class Dest>
+using SwapContainer_t = typename SwapContainer<Source, Dest>::type;
+
+/////////////////////////////////////////////////////////////////////////////
+//                                 Contains                                //
+/////////////////////////////////////////////////////////////////////////////
+template <class List, class T> struct Contains : std::false_type {};
+
+template <template <class...> class List, class T, class U, class... Other>
+struct Contains<List<U, Other...>, T> : Contains<List<Other...>, T> {};
+
+template <template <class...> class List, class T, class... Other>
+struct Contains<List<T, Other...>, T> : std::true_type {};
+
+template <template <class...> class List, class T>
+struct Contains<List<>, T> : std::false_type {};
+
+template <class List, class T>
+constexpr bool Contains_v = Contains<List, T>::value;
+
+using AllNonConstSignedRangeView_t =
+    meta::TypeList<atb_8RangeView, atb_16RangeView, atb_32RangeView,
+                   atb_64RangeView>;
+
+using AllConstSignedRangeView_t =
+    meta::TypeList<atb_Const8RangeView, atb_Const16RangeView,
+                   atb_Const32RangeView, atb_Const64RangeView>;
+
+using AllSignedRangeView_t =
+    meta::Combine_t<AllNonConstSignedRangeView_t, AllConstSignedRangeView_t>;
+
+using AllNonConstUnsignedRangeView_t =
+    meta::TypeList<atb_u8RangeView, atb_u16RangeView, atb_u32RangeView,
+                   atb_u64RangeView>;
+
+using AllConstUnsignedRangeView_t =
+    meta::TypeList<atb_Constu8RangeView, atb_Constu16RangeView,
+                   atb_Constu32RangeView, atb_Constu64RangeView>;
+
+using AllUnsignedRangeView_t = meta::Combine_t<AllNonConstUnsignedRangeView_t,
+                                               AllConstUnsignedRangeView_t>;
+
+using AllIntRangeView_t =
+    meta::Combine_t<AllSignedRangeView_t, AllUnsignedRangeView_t>;
+
+using AllRangeView_t = meta::Combine_t<AllIntRangeView_t, atb_CharRangeView>;
+
+/////////////////////////////////////////////////////////////////////////////
+//                               IsRangeView                               //
+/////////////////////////////////////////////////////////////////////////////
+template <class T> struct IsRangeView : Contains<AllRangeView_t, T> {};
+template <class T> constexpr bool IsRangeView_v = IsRangeView<T>::value;
+
+} // namespace meta
+
 template <class T, class..., class IntegerType = std::decay_t<T>,
           std::enable_if_t<std::is_integral_v<IntegerType>, bool> = true>
 constexpr auto ToHexString(T &&val)
@@ -37,20 +130,8 @@ constexpr auto ToHexString(T &&val)
 template <std::size_t N>
 constexpr auto ToStringView(const std::array<char, N> &array)
     -> std::string_view {
-  return std::string_view(array.data(), array.size());
+  return std::string_view(array.data(), array.size() - 1);
 }
-
-namespace meta {
-
-template <class T, class = void> struct IsRangeView : std::false_type {};
-template <class T>
-struct IsRangeView<T, std::void_t<decltype(std::declval<T>().data),
-                                  decltype(std::declval<T>().size)>>
-    : std::is_pointer<decltype(std::declval<T>().data)> {};
-
-template <class T> constexpr bool IsRangeView_v = IsRangeView<T>::value;
-
-} // namespace meta
 
 template <class T, class..., class DecayType = std::decay_t<T>,
           std::enable_if_t<meta::IsRangeView_v<DecayType>, bool> = true>
