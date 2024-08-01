f65fc48a3d70b9e031614192fc7c83381df7fdc1
Arthur Valiente <valiente.arthur@gmail.com>
2024-08-01 15:12:25 +0200

    Add memory/view

---
+++ tests/atb/helper/Memory.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "Core.hpp"
+
+#include "atb/memory/view.h"
+
+#include <ostream>
+#include <string_view>
+#include <type_traits>
+
+inline std::ostream &operator<<(std::ostream &os, atb_MemView mem) noexcept {
+
+  os << helper::MakeStringFromFmt(atb_MemView_Fmt, atb_MemView_FmtVaArg(mem));
+
+  if (atb_MemView_IsInvalid(mem)) {
+    os << " => /!\\ INVALID";
+
+  } else if ((mem.data != nullptr) and (mem.size > 0)) {
+    os << " => 0x";
+
+    char byte_str[3] = {'\0'};
+    const uint8_t *byte = nullptr;
+    atb_MemView_ForEachAs(uint8_t, byte, mem) {
+
+      helper::IntToStr(byte_str, byte_str + 2, *byte,
+                       {
+                           /*.base= */ 16,
+                           /*.fill = */ '0',
+                       });
+
+      os << " " << byte_str;
+    }
+  }
+
+  return os;
+}
+
+namespace helper {
+
+DefineFieldsMatchFor(atb_MemView, 2, data, size);
+
+} // namespace helper
