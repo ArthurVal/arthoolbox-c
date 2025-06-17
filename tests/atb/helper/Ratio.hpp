f0db7c19af959a492c77e36d3603d3a7cbaaa6b1
ArthurVal <valiente.arthur@gmail.com>
2025-06-17 10:05:26 +0200

    Add helper/Ratio for testing

---
+++ tests/atb/helper/Ratio.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <ostream>
+
+#include "Core.hpp"
+#include "atb/ratio.h"
+
+inline auto operator<<(std::ostream &os, atb_Ratio ratio) -> std::ostream & {
+  return os << "Ratio{" << ratio.num << ", " << ratio.den << "}";
+}
+
+constexpr auto operator==(atb_Ratio lhs, atb_Ratio rhs) -> bool {
+  return (lhs.num == rhs.num) && (lhs.den == rhs.den);
+}
+
+constexpr auto operator!=(atb_Ratio lhs, atb_Ratio rhs) -> bool {
+  return !(lhs == rhs);
+}
+
+constexpr auto ToString(atb_Ratio_Compare_Result cmp_res) -> std::string_view {
+  switch (cmp_res) {
+    case atb_Ratio_Compare_EQUAL: return "EQUAL";
+    case atb_Ratio_Compare_LESS: return "LESS";
+    case atb_Ratio_Compare_GREATER: return "GREATER";
+  }
+  return "UNKNOWN";
+}
+
+inline auto operator<<(std::ostream &os,
+                       atb_Ratio_Compare_Result cmp_res) -> std::ostream & {
+  os << std::quoted(ToString(cmp_res)) << " (" << static_cast<int>(cmp_res)
+     << ")";
+  return os;
+}
+
+namespace helper {
+
+DefineFieldsMatchFor(atb_Ratio, 2, num, den);
+
+} // namespace helper
