18a89e7228869fdcccb6be10fe3dd4c5ed204830
Arthur Valiente <arthur.valiente@erems.fr>
2025-07-07 10:04:17 +0200

    Remove the GNU statement expression for RetryCall

--- include/atb/time.h
+++ include/atb/time.h
@@ -64,90 +64,6 @@ static inline bool atb_timespec_Le(struct timespec lhs, struct timespec rhs);
 
 /* Utils functions ***********************************************************/
 /**
- *  \brief Call 'predicate' expression until it evaluates to true or up to
- *         'count' times, separated by 'delay' in between each retry calls
- *
- *  \param[in] predicate Expression expected to evaluate to a boolean expression
- *             , that will be call every time until it returns TRUE or the retry
- *             count is reached.
- *  \param[in] count (size_t) Number of retry to do when predicate fails
- *  \param[in] delay (struct timespec) Delay in between each failed call
- *
- *  \return True when the predicate succeed BEFORE we exceeded count,
- *          otherwise, false if the retry count has been reached and predicate
- *          failed everytime.
- *
- *  \note Use the STATEMENT EXPRESSION (using braces {} inside parenthesis) GNU
- *        extension, only available with GCC and Clang. It allows you to use
- *        loops/switches/local variables inside a macro expression, serving as
- *        'safe macro' (can test input params types and only evaluate them once)
- *        and have a return value. See
- *        https://gcc.gnu.org/onlinedocs/gcc-3.1.1/gcc/Statement-Exprs.html
- *
- *  \code
- *  int main() {
- *    int i = 0;
- *
- *    if(atb_Time_retryCallExpr(
- *        (printf("%d\n", i), (i++ == 5)),
- *        20,
- *        atb_timespec_fromMs(5)
- *      )) {
- *      puts("Expression succeed");
- *    } else {
- *      puts("Max count reached");
- *    }
- *    // This expression above ouputs:
- *    // 0
- *    // 1
- *    // 2
- *    // 3
- *    // 4
- *    // 5
- *    // Expression succeed
- *
- *    i = 0;
- *    if(atb_Time_retryCallExpr(
- *        (printf("%d\n", i), (i++ == 5)),
- *        4,
- *        atb_timespec_fromMs(5)
- *      )) {
- *      puts("Expression succeed");
- *    } else {
- *      puts("Max count reached");
- *    }
- *    // This expression above ouputs:
- *    // 0
- *    // 1
- *    // 2
- *    // 3
- *    // Max count reached
- *
- *    return 0;
- *  }
- *  \endcode
- */
-#define atb_Time_RetryCallExpr(predicate, count, delay)                        \
-  ({                                                                           \
-    /* This is used as 'safe macro' in order to check input types */           \
-    /* Also all variables declared inside the macro expression are */          \
-    /* surounded by '_' in order to avoid shadowing with user code. */         \
-    struct timespec _delay_ = (delay);                                         \
-    bool _succeed_ = false;                                                    \
-    /* Comparison here is used to secure negative timespecs ... */             \
-    size_t _count_ =                                                           \
-        (atb_timespec_Le(_delay_, atb_timespec_FromNs(0)) ? 0 : (count));      \
-                                                                               \
-    while (!(_succeed_ = (predicate)) && (_count_ != 0)) {                     \
-      --_count_;                                                               \
-      if (nanosleep(&_delay_, NULL) == -1) {                                   \
-        /* TODO: Handle EINTR when receiving signals ? */                      \
-      }                                                                        \
-    }                                                                          \
-    _succeed_; /* return value */                                              \
-  })
-
-/**
  *  \brief Argument used to describe the predicate (function ptr + args) from
  *         _RetryCall()
  */
@@ -156,9 +72,11 @@ struct atb_Time_RetryPredicate {
   void *args;               /*!< Argument forwarded to the function above */
 };
 
+typedef struct atb_Time_RetryPredicate atb_Time_RetryPredicate;
+
 /**
- *  \brief Call 'predicate' until it returns true or up to 'count' times,
- *         separated by 'delay' in between each retry calls
+ *  \brief Call 'predicate'. If it fails, retry to call it until succeed or
+ *         up to 'count' times separated by 'delay' in between each retries
  *
  *  \param[in] predicate Function that will be call every time until it returns
  *             TRUE or the retry count is reached.
@@ -168,6 +86,10 @@ struct atb_Time_RetryPredicate {
  *  \return True when the predicate succeed BEFORE we exceeded count,
  *          otherwise, false if the retry count has been reached and predicate
  *          failed everytime.
+ *
+ *  \pre predicate.function != NULL
+ *
+ *  \note count will be zeroed when delay is <= 0
  */
 static inline bool atb_Time_RetryCall(struct atb_Time_RetryPredicate predicate,
                                       size_t count, struct timespec delay);
@@ -280,15 +202,22 @@ static inline bool atb_timespec_Le(struct timespec lhs, struct timespec rhs) {
 
 static inline bool atb_Time_RetryCall(struct atb_Time_RetryPredicate predicate,
                                       size_t count, struct timespec delay) {
-  if(predicate.function == NULL)
-    return false;
+  assert(predicate.function != NULL);
 
-  /* We use retryCallExpr here in order to avoid code duplication...
-   * TODO: Find a way to re-use the GNU style stuff without triggering
-   *       warnings ?
-   */
-  return atb_Time_RetryCallExpr(predicate.function(predicate.args), count,
-                                delay);
+  /* Any negative timespecs cancel the retries */
+  if (atb_timespec_Le(delay, atb_timespec_From(0, atb_sec()))) {
+    count = 0;
+  }
+
+  bool succeeded = predicate.function(predicate.args);
+  while ((!succeeded) && (count-- != 0)) {
+    /* TODO: Handle EINTR when receiving signals ? */
+    if (nanosleep(&delay, NULL) == -1) break;
+
+    succeeded = predicate.function(predicate.args);
+  }
+
+  return succeeded;
 }
 
 #if defined(__cplusplus)
