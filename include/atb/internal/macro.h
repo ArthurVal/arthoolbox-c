a1b219a61374e855f3190f52f5461df377ba5cc3
ArthurVal <valiente.arthur@gmail.com>
2025-07-21 16:43:21 +0200

    Remove _Macro in internal_ stuff

--- include/atb/internal/macro.h
+++ include/atb/internal/macro.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#define atb_internal_Macro_ARGN_20 \
+#define atb_internal_ARGN_20 \
   20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
 
-#define atb_internal_Macro_ARGN_IMPL_20(_0, _1, _2, _3, _4, _5, _6, _7, _8,    \
+#define atb_internal_ARGN_IMPL_20(_0, _1, _2, _3, _4, _5, _6, _7, _8,    \
                                         _9, _10, _11, _12, _13, _14, _15, _16, \
                                         _17, _18, _19, _20, N, ...)            \
   N
 
-#define atb_internal_Macro_ARGN(...) \
-  atb_internal_Macro_ARGN_IMPL_20(__VA_ARGS__)
+#define atb_internal_ARGN(...) \
+  atb_internal_ARGN_IMPL_20(__VA_ARGS__)
--- include/atb/macro.h
+++ include/atb/macro.h
@@ -6,4 +6,4 @@
  *  \return Get the size of the given __VA_ARGS__ (up to 20)
  */
 #define atb_VA_ARGN(...) \
-  atb_internal_Macro_ARGN(_, ##__VA_ARGS__, atb_internal_Macro_ARGN_20)
+  atb_internal_ARGN(_, ##__VA_ARGS__, atb_internal_ARGN_20)
