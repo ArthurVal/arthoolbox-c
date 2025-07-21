a5cf2100426747d180387966eb5341159454733e
ArthurVal <valiente.arthur@gmail.com>
2025-07-21 16:39:19 +0200

    Rename atb_Macro_ARGN to atb_VA_ARGN

--- tests/atb/test_macro.cpp
+++ tests/atb/test_macro.cpp
@@ -4,11 +4,11 @@
 namespace {
 
 TEST(TestAtbMacro, ARGN) {
-  EXPECT_EQ(0, atb_Macro_ARGN());
-  EXPECT_EQ(1, atb_Macro_ARGN(1));
-  EXPECT_EQ(2, atb_Macro_ARGN(1, 1));
-  EXPECT_EQ(2, atb_Macro_ARGN(1, 'v'));
-  EXPECT_EQ(5, atb_Macro_ARGN(1, 'v', 1, 'v', 1));
+  EXPECT_EQ(0, atb_VA_ARGN());
+  EXPECT_EQ(1, atb_VA_ARGN(1));
+  EXPECT_EQ(2, atb_VA_ARGN(1, 1));
+  EXPECT_EQ(2, atb_VA_ARGN(1, 'v'));
+  EXPECT_EQ(5, atb_VA_ARGN(1, 'v', 1, 'v', 1));
 }
 
 } // namespace
