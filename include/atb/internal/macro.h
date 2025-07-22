cce55ed23e7648c73e8a5ac5e83a9fa3a51b33d3
ArthurVal <valiente.arthur@gmail.com>
2025-07-22 08:44:36 +0200

    Add atb_TKN_CONCAT() in macro.h

--- include/atb/internal/macro.h
+++ include/atb/internal/macro.h
@@ -3,10 +3,36 @@
 #define atb_internal_ARGN_20 \
   20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
 
-#define atb_internal_ARGN_IMPL_20(_0, _1, _2, _3, _4, _5, _6, _7, _8,    \
-                                        _9, _10, _11, _12, _13, _14, _15, _16, \
-                                        _17, _18, _19, _20, N, ...)            \
+#define atb_internal_ARGN_IMPL_20(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, \
+                                  _11, _12, _13, _14, _15, _16, _17, _18, _19, \
+                                  _20, N, ...)                                 \
   N
 
-#define atb_internal_ARGN(...) \
-  atb_internal_ARGN_IMPL_20(__VA_ARGS__)
+#define atb_internal_ARGN(...) atb_internal_ARGN_IMPL_20(__VA_ARGS__)
+
+#define atb_internal_TKN_CONCAT_IMPL(a, b) a##b
+
+#define atb_internal_TKN_CONCAT_0()
+#define atb_internal_TKN_CONCAT_1(a) a
+#define atb_internal_TKN_CONCAT_2(a, b) atb_internal_TKN_CONCAT_IMPL(a, b)
+#define atb_internal_TKN_CONCAT_3(a, ...) \
+  atb_internal_TKN_CONCAT_2(a, atb_internal_TKN_CONCAT_2(__VA_ARGS__))
+#define atb_internal_TKN_CONCAT_4(a, ...) \
+  atb_internal_TKN_CONCAT_2(a, atb_internal_TKN_CONCAT_3(__VA_ARGS__))
+#define atb_internal_TKN_CONCAT_5(a, ...) \
+  atb_internal_TKN_CONCAT_2(a, atb_internal_TKN_CONCAT_4(__VA_ARGS__))
+#define atb_internal_TKN_CONCAT_6(a, ...) \
+  atb_internal_TKN_CONCAT_2(a, atb_internal_TKN_CONCAT_5(__VA_ARGS__))
+#define atb_internal_TKN_CONCAT_7(a, ...) \
+  atb_internal_TKN_CONCAT_2(a, atb_internal_TKN_CONCAT_6(__VA_ARGS__))
+#define atb_internal_TKN_CONCAT_8(a, ...) \
+  atb_internal_TKN_CONCAT_2(a, atb_internal_TKN_CONCAT_7(__VA_ARGS__))
+#define atb_internal_TKN_CONCAT_9(a, ...) \
+  atb_internal_TKN_CONCAT_2(a, atb_internal_TKN_CONCAT_8(__VA_ARGS__))
+#define atb_internal_TKN_CONCAT_10(a, ...) \
+  atb_internal_TKN_CONCAT_2(a, atb_internal_TKN_CONCAT_9(__VA_ARGS__))
+
+#define atb_internal_TKN_CONCAT_N_IMPL(N, ...) \
+  atb_internal_TKN_CONCAT_##N(__VA_ARGS__)
+#define atb_internal_TKN_CONCAT_N(N, ...) \
+  atb_internal_TKN_CONCAT_N_IMPL(N, __VA_ARGS__)
--- include/atb/macro.h
+++ include/atb/macro.h
@@ -3,7 +3,13 @@
 #include "internal/macro.h"
 
 /**
- *  \return Get the size of the given __VA_ARGS__ (up to 20)
+ *  \return Get the size of the given __VA_ARGS__ (up to 20 tokens)
  */
 #define atb_VA_ARGN(...) \
   atb_internal_ARGN(_, ##__VA_ARGS__, atb_internal_ARGN_20)
+
+/**
+ *  \brief Create a token by concatenating the given args (up to 10 tokens)
+ */
+#define atb_TKN_CONCAT(...) \
+  atb_internal_TKN_CONCAT_N(atb_VA_ARGN(__VA_ARGS__), __VA_ARGS__)
