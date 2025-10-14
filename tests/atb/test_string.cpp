655c73570376c2ef0802c8fe10b689d7e1ed9cdc
ArthurVal <valiente.arthur@gmail.com>
2025-10-14 13:32:33 +0200

    Fix wrong test suite names

--- tests/atb/test_string.cpp
+++ tests/atb/test_string.cpp
@@ -178,14 +178,14 @@ TEST(AtbStringTest, FromInt) {
                    }));
 }
 
-TEST(AtbStringDeathTest, ToIntBase) {
+TEST(AtbStringDeathTest, GetIntBase) {
   EXPECT_DEBUG_DEATH(
       atb_String_GetIntBase(atb_StrView_From_StrLiteral("3058u018u"), nullptr,
                             nullptr, K_ATB_ERROR_IGNORED),
       "base != NULL");
 }
 
-TEST(AtbStringTest, ToIntBase) {
+TEST(AtbStringTest, GetIntBase) {
   atb_Error err;
   atb_StrView remaining = K_ATB_ANYSPAN_INVALID;
 
